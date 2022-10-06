ac388f2fc28a090f64158b1fa996458048155cbe
vomikan <nikolay.akimov@gmail.com>
2022-10-06 11:47:24 +0300

    update

---
+++ SYSTEM/get_group_id/get_group_id.groovy
@@ -0,0 +1,5 @@
+﻿def flowFile = session.get()
+if(!flowFile) return
+processGroupId = context.procNode?.processGroupIdentifier ?: 'unknown'
+flowFile = session.putAttribute(flowFile, 'processGroupId', processGroupId)
+session.transfer(flowFile, REL_SUCCESS)
